ad56427cc12a8901580785ccbd78982d624630ac
Muhammad Asad Baidar <asadbaidar@gmail.com>
2024-12-01 23:59:18 +0100

    local common and locale packages

---
+++ packages/common/lib/utils/http_client.dart
@@ -0,0 +1,16 @@
+import 'package:common/common.dart';
+
+class HttpClientImpl extends HttpClient {
+  HttpClientImpl({required this.environment})
+      : super(clientConfig: environment.config);
+
+  final Environment environment;
+
+  /// Headers with api token
+  @override
+  Map<String, String>? get headers => {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'apikey': environment.apiToken,
+      };
+}
