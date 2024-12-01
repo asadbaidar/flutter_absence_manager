dd4aab30e7e13b5e49a43f522a8004d89ab674d3
Muhammad Asad Baidar <asadbaidar@gmail.com>
2024-12-01 05:31:38 +0100

    created flutter base app with android, ios, and web platform

---
+++ app/crewmeister/lib/environment.dart
@@ -0,0 +1,53 @@
+import 'package:core/core.dart';
+
+class AppEnvironment {
+  const AppEnvironment._(); // coverage:ignore-line
+
+  /// The name of the customer
+  static const name = 'crewmeister';
+
+  /// The app url scheme
+  static const scheme = 'com.asadbaidar.crewmeister';
+
+  /// The host of the api
+  static const host = 'https://fhmcmatrhxmsmfmmwrth.supabase.co/rest/v1/rpc';
+
+  /// The api token
+  static const apiToken = '';
+
+  /// The production environment
+  static const prod = Environment(
+    name: name,
+    scheme: scheme,
+    flavor: Flavor.prod,
+    host: host,
+    apiToken: apiToken,
+  );
+
+  /// The staging environment
+  static const stage = Environment(
+    name: name,
+    scheme: scheme,
+    flavor: Flavor.stage,
+    host: host,
+    apiToken: apiToken,
+  );
+
+  /// The QA environment
+  static const qa = Environment(
+    name: name,
+    scheme: scheme,
+    flavor: Flavor.qa,
+    host: host,
+    apiToken: apiToken,
+  );
+
+  /// The development environment
+  static const dev = Environment(
+    name: name,
+    scheme: scheme,
+    flavor: Flavor.dev,
+    host: host,
+    apiToken: apiToken,
+  );
+}
